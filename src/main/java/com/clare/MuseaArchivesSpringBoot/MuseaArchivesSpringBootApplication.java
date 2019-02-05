ad60c809a84873399f0b030b8343f741e3158913
clare <kirabo646@googlemail.com>
2019-02-05 14:41:21 +0000

    first commit

---
+++ src/main/java/com/clare/MuseaArchivesSpringBoot/MuseaArchivesSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.clare.MuseaArchivesSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseaArchivesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseaArchivesSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/clare/MuseaArchivesSpringBoot/MuseaArchivesSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.clare.MuseaArchivesSpringBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MuseaArchivesSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
