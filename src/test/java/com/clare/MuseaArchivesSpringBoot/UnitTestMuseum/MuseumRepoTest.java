4cf36470a985f1a5f4b1f8ca1e55c668c4519495
clare <kirabo646@googlemail.com>
2019-02-13 13:38:00 +0000

    Added repo test for get musuem by id.

--- src/test/java/com/clare/MuseaArchivesSpringBoot/UnitTestMuseum/MuseumRepoTest.java
+++ src/test/java/com/clare/MuseaArchivesSpringBoot/UnitTestMuseum/MuseumRepoTest.java
@@ -1,15 +1,22 @@
 package com.clare.MuseaArchivesSpringBoot.UnitTestMuseum;
 
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.clare.MuseaArchivesSpringBoot.MuseaArchivesSpringBootApplication;
+import com.clare.MuseaArchivesSpringBoot.Model.MuseumModel;
 import com.clare.MuseaArchivesSpringBoot.Repository.MuseumRepo;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = MuseaArchivesSpringBootApplication.class)
 @DataJpaTest
 public class MuseumRepoTest {
 
@@ -18,5 +25,30 @@ public class MuseumRepoTest {
 	
 	@Autowired
 	private MuseumRepo myRepo;
+	
+	@Test
+	public void getAMuseumById() {
+		MuseumModel model1 = new MuseumModel(
+				
+				"Test Museum",
+				"address 1",
+				"address 2",
+				"Manchester",
+				"ABC 123",
+				"Test@gmail.com",
+				"02020 02020",
+				"Pay Fee",
+				"www.doesntExist.com",
+				"Historic"
+				);
+		
+		entityManager.persist(model1);
+		entityManager.flush();
+		assertTrue(myRepo.findById(model1.getMuseum_ID()).isPresent());
+		
+	}
+	
+
+	
 	
 }
